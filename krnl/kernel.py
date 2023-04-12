830fefbb646a5318e8e1aea3f0ef1a5afca6ea4c
Mohammad <mim.seyedi@gmail.com>
2023-04-12 19:40:45 +0330

    The find_prg function was modified to find built-in programs.

--- krnl/kernel.py
+++ krnl/kernel.py
@@ -21,19 +21,34 @@ For more information: https://github.com/mimseyedi/gerdoo#kernel
 import os
 import sys
 import json
+import typing
 from pathlib import Path
 
+from bins.back import back as back_program
+from bins.bash import bash as bash_program
+from bins.goto import goto as goto_program
+from bins.home import home as home_program
+from bins.logs import logs as logs_program
+from bins.path import path as path_program
+from bins.void import void as void_program
 
-def find_prg(program: str) -> bool|Path:
+
+def find_prg(program: str) -> bool|Path|typing.Callable:
     """
     The task of this function is to find programs and return their path.
 
     :param program: Programs name in string format.
-    :return: False|Path
+    :return: False|Path|Callable
     """
 
-    # Built-in prgoram path:
-    bins: Path = Path(Path(__file__).parent, 'bins/', program)
+    # Built-in prgoram func:
+    bins: dict = {back_program.back.__name__: back_program.back,
+                  bash_program.bash.__name__: bash_program.bash,
+                  goto_program.goto.__name__: goto_program.goto,
+                  home_program.home.__name__: home_program.home,
+                  logs_program.logs.__name__: logs_program.logs,
+                  path_program.path.__name__: path_program.path,
+                  void_program.void.__name__: void_program.void}
 
     # Standard program path:
     stnd: Path = Path(Path(__file__).parent.parent, 'prgs/stnd/', program)
@@ -41,7 +56,7 @@ def find_prg(program: str) -> bool|Path:
     # External program path:
     extn: Path = Path(Path(__file__).parent.parent, 'prgs/extn/', program)
 
-    return bins if bins.exists() else (stnd if stnd.exists() else (extn if extn.exists() else False))
+    return bins[program] if program in bins else (stnd if stnd.exists() else (extn if extn.exists() else False))
 
 
 def read_pif(pif_path: str|Path) -> bool|dict:
