8cda5578a5af5eb1232022368a3a69f4dd26da42
Mohammad <mim.seyedi@gmail.com>
2023-04-20 01:31:34 +0330

    The main and running program of gerdoo was written.

---
+++ gerdoo.py
@@ -0,0 +1,19 @@
+"""
+                     _
+                    | |
+   __ _  ___ _ __ __| | ___   ___
+  / _` |/ _ \ '__/ _` |/ _ \ / _ \
+ | (_| |  __/ | | (_| | (_) | (_) |
+  \__, |\___|_|  \__,_|\___/ \___/
+   __/ |
+  |___/
+
+"""
+
+import os
+import sys
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    os.system(f"{sys.executable} {Path('shll', 'shell.py')}")
