2f4a2e537deb2eb818e4c3eb52b3acfbc64a7176
Mohammad <mim.seyedi@gmail.com>
2025-11-18 20:27:41 +0330

    Transactions URLs defined

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,16 @@ from .views.dashboard import (
     get_annual_chart_data,
     category_tag_report_api,
 )
+# Transactions
+from .views.transactions import (
+    transactions,
+    add_transaction,
+    delete_transaction,
+    get_categories_by_kind,
+    get_transactions_by_month,
+    get_transaction_details_by_id,
+)
+
 
 urlpatterns = [
     # Home
@@ -30,4 +40,12 @@ urlpatterns = [
     path('api/monthly-data/', monthly_data_api, name='monthly_data_api'),
     path('api/annual-data/', get_annual_chart_data, name='get_annual_chart_data'),
     path('api/reports/category_tags/', category_tag_report_api, name='category_tag_report_api'),
+
+    # Transactions
+    path('transactions', transactions, name="transactions"),
+    path('transactions/add/', add_transaction, name='add_transaction'),
+    path('transactions/delete/', delete_transaction, name='delete_transaction'),
+    path('get_categories/', get_categories_by_kind, name='get_categories_by_kind'),
+    path('transactions/details/', get_transaction_details_by_id, name='get_transaction_details'),
+    path('transactions/get_by_month/', get_transactions_by_month, name='get_transactions_by_month'),
 ]
