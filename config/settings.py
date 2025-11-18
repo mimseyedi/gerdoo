ab66d45f01c98fce73526315ebd6ee1db24d0208
Mohammad <mim.seyedi@gmail.com>
2025-11-18 17:47:22 +0330

    The statics path has been defined and the relevant settings have been made

--- config/settings.py
+++ config/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = Path(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = Path(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR, "statics"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
