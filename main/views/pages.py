03ab75d75af1829c8bef3f6363a5dc899737cb2e
Mohammad <mim.seyedi@gmail.com>
2025-11-18 18:12:03 +0330

    Logic related to displaying specific pages was implemented

---
+++ main/views/pages.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+
+
+def custom_page_not_found(request, *args, **kwargs):
+    return render(request, 'main/404.html', {}, status=404)
+
+
+def access_denied_view(request):
+    return render(request, 'main/no_auth.html', {})
+
+
+def home(request):
+    return redirect('login')
