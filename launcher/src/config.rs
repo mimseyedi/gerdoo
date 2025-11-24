95dde63568fa509f5dbb720986d7fceb9770f993
Mohammad <mim.seyedi@gmail.com>
2025-11-24 13:34:41 +0330

    The `config` module was implemented

---
+++ launcher/src/config.rs
@@ -0,0 +1,78 @@
+use std::path::{
+    PathBuf,
+};
+use directories::{
+    ProjectDirs,
+};
+use serde::{
+    Deserialize,
+    Serialize,
+};
+
+
+#[derive(
+    Debug,
+    Clone,
+    Serialize,
+    Deserialize,
+)]
+pub struct LauncherConfig {
+    pub server_pid: Option<u32>,
+    pub current_version: String,
+    pub app_path: PathBuf,
+}
+
+
+impl LauncherConfig {
+    const QUA: &'static str = "app";
+    const ORG: &'static str = "Gerdoo";
+    const APP: &'static str = "Launcher";
+
+    pub fn load() -> Result<Self, String> {
+        let path = Self::prepare()?;
+        if !path.exists() {
+            let default_config = Self::default();
+            default_config.save()?;
+            return Ok(default_config);
+        }
+        let file_content = std::fs::read_to_string(path)
+            .map_err(|e| format!("Error: {}", e))?;
+        serde_json::from_str(&file_content)
+            .map_err(|e| format!("Error: {}", e))
+    }
+
+    pub fn save(&self) -> Result<(), String> {
+        let path = Self::prepare()?;
+        let json_content = serde_json::to_string_pretty(self)
+            .map_err(|e| format!("Error: {}", e))?;
+        std::fs::write(path, json_content)
+            .map_err(|e| format!("Error: {}", e))
+    }
+
+    fn prepare() -> Result<PathBuf, String> {
+        let launcher_dir = Self::get_launcher_sys_path()?;
+        let config_dir = launcher_dir.config_dir();
+        std::fs::create_dir_all(config_dir)
+            .map_err(|e| format!("Error: {}", e))?;
+        Ok(config_dir.join("config.json"))
+    }
+
+    fn get_launcher_sys_path() -> Result<ProjectDirs, String> {
+        ProjectDirs::from(
+            Self::QUA,
+            Self::ORG,
+            Self::APP,
+        ).ok_or_else(|| "Not found!".to_owned())
+    }
+}
+
+
+impl Default for LauncherConfig {
+    fn default() -> Self {
+        Self {
+            server_pid: None,
+            current_version: "0.0.0".to_owned(),
+            app_path: PathBuf::from("./gerdoo"),
+        }
+    }
+}
