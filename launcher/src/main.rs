b11927672f13523e45ed9fa9f8800d43ed9f2239
Mohammad <mim.seyedi@gmail.com>
2025-11-24 13:33:02 +0330

    General and initial implementation of the main program file

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    
+mod gui;
+mod setup;
+mod server;
+mod config;
+mod update;
+
+
+use config::{
+    LauncherConfig,
+};
+
+
+#[tokio::main]
+async fn main() -> eframe::Result<()> {
+    let cfg = LauncherConfig::load()
+        .unwrap_or_else( |e| {
+            eprintln!("Error: {}", e);
+            LauncherConfig::default()
+        });
+    gui::run_gui(cfg).await
 }
