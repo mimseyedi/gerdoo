f75d20d27e4115c9a3bdf610f3b6df874096a4aa
Mohammad <mim.seyedi@gmail.com>
2025-11-26 01:07:29 +0330

    The main execution settings for the updater program have been made

--- launcher/src/main.rs
+++ launcher/src/main.rs
@@ -1,21 +1,57 @@
+use eframe::{self, NativeOptions};
+use std::sync::{Arc, Mutex};
+
+
 mod gui;
 mod setup;
 mod server;
 mod config;
 mod update;
+mod updater;
 
-use egui::Key::A;
-use config::{
-    AppConfig,
-};
+
+use config::AppConfig;
+use update::UpdateManager;
+use updater::UpdaterApp;
+use gui::run_gui;
 
 
 #[tokio::main]
 async fn main() -> eframe::Result<()> {
-    let cfg = AppConfig::load()
+    let initial_config = AppConfig::load()
         .unwrap_or_else( |e| {
             eprintln!("Error: {}", e);
             AppConfig::default()
         });
-    gui::run_gui(cfg).await
+
+    let update_manager = UpdateManager::new(
+        initial_config.app_path.clone(),
+        initial_config.current_version.clone(),
+    );
+
+    let updater_options = eframe::NativeOptions {
+        centered: true,
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 100.0])
+            .with_resizable(false)
+            .with_title("Gerdoo Updater")
+            .with_always_on_top(),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Gerdoo Updater",
+        updater_options,
+        Box::new(|_cc| {
+            Ok(Box::new(UpdaterApp::new(update_manager)))
+        }),
+    )?;
+
+    let final_config = AppConfig::load()
+        .unwrap_or_else( |e| {
+            eprintln!("Error: {}", e);
+            initial_config
+        });
+
+    gui::run_gui(final_config).await
 }
