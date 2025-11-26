aacc2ef04a2f146f97292688be968b67a2719055
Mohammad <mim.seyedi@gmail.com>
2025-11-26 19:16:27 +0330

    Importing was fixed

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from ..main.views.pages import (
+from main.views.pages import (
     access_denied_view,
     custom_page_not_found,
 )
